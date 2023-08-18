71e74e800ba8dcad4b0b540d192b6f8385dec40a
Sahil Abrar <zeroxkhan2@gmail.com>
2023-08-18 11:50:48 +0600

    new commit

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ function calculateTriangleArea() {
   const heightValueText = heightField.value;
   const height = parseFloat(heightValueText);
 
+  // validate input------------
+  
+  if (isNaN(base) || isNaN(height)) {
+    alert('Please insert a number');
+    return;
+  }
 
   const area = 0.5 * base * height;
   
   const areaSpan = document.getElementById('triangle-area');
   areaSpan.innerText = area;
+  
+   addToCalculationEntry('Triangle', area);
 }
 
 function calculateRectangleArea() {
@@ -23,10 +31,19 @@ const lengthField = document.getElementById('rectangle-length');
   const lengthValueText = lengthField.value;
   const length = parseFloat(lengthValueText);
   
+// validate input------------
+  
+  if (isNaN(width) || isNaN(length)) {
+    alert('Please insert a number');
+    return;
+  }
+
   const area = width * length;
 
   const rectAreaSpan = document.getElementById('rectangle-area');
   rectAreaSpan.innerText = area;
+
+   addToCalculationEntry('Rectangle', area);
 }
 
 
@@ -37,7 +54,16 @@ function calculateParallelogramArea() {
 
   const area = base * height;
 
+  // validate input------------
+  
+  if (isNaN(base) || isNaN(height)) {
+    alert('Please insert a number');
+    return;
+  }
+
   setElementInnerText('parallelogram-area', area);
+
+  addToCalculationEntry('parallelogram', area);
 }
 
 function calculateEllipseArea() {
@@ -45,11 +71,20 @@ function calculateEllipseArea() {
 
   const minor = getInputValue('ellipse-radius2');
 
+  // validate input------------
+  
+  if (isNaN(major) || isNaN(minor)) {
+    alert('Please insert a number');
+    return;
+  }
+
   const area = 3.1416 * major * minor;
 
   const areaTwoDecimal = area.toFixed(2);
 
   setElementInnerText('ellipse-area', areaTwoDecimal);
+
+  addToCalculationEntry('Ellipse', areaTwoDecimal);
 }
 // reusable functions-------------------------------------
 
@@ -67,4 +102,19 @@ function getInputValue(fieldId) {
 function setElementInnerText(elementId, area) {
   const element = document.getElementById(elementId)
   element.innerText = area;
+}
+
+
+// add to calculation entry----------------------------------------
+
+function addToCalculationEntry(areaType, area) {
+  const calculationEntry = document.getElementById('calculation-entry');
+
+  const count = calculationEntry.childElementCount;
+
+  const para = document.createElement('p');
+  para.classList.add('my-4');
+  para.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class = "btn btn-success btn-small">Convert</button>`;
+
+  calculationEntry.appendChild(para);
 }
